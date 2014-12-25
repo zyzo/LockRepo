b3a6a0a1d7a4d5857ada7c691296c9939bb19bae
zyzo <zyzo@github.com>
2014-12-25 21:52:22 +0100

    add view thanks to Swing UI jetbrains jgoodies

---
+++ src/main/java/dha/lockrepo/view/swing/MainWindow.java
@@ -0,0 +1,14 @@
+package dha.lockrepo.view.swing;
+
+import javax.swing.*;
+
+/**
+ * Created by zyzo on 25/12/14.
+ */
+public class MainWindow {
+    private JPanel mainPanel;
+    private JList list2;
+    private JButton addBtn;
+    private JTree tree1;
+
+}
