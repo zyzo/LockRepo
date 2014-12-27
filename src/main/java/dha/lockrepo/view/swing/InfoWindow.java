aee2c72af06606938d5e1616b2391bb1e527e2ae
zyzo <zyzo@github.com>
2014-12-27 14:30:23 +0100

    continue view work

--- src/main/java/dha/lockrepo/view/swing/InfoWindow.java
+++ src/main/java/dha/lockrepo/view/swing/InfoWindow.java
@@ -1,15 +1,29 @@
 package dha.lockrepo.view.swing;
 
-import javax.swing.*;
+import dha.lockrepo.core.domains.TopSecretBE;
+import dha.lockrepo.core.domains.TopSecretPieceBE;
 
-/**
- * Created by zyzo on 26/12/14.
- */
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class InfoWindow {
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JTextField usernameTxtField;
+    private JTextField passwordTxtField;
+    private JTextField descriptionTxtField;
+    private JPanel formPanel;
+    private JButton saveBtn;
+    /**
+     * Item by title
+     */
+    private TopSecretPieceBE item;
+
+    public InfoWindow() {
+        usernameTxtField.setColumns(20);
+        passwordTxtField.setColumns(20);
+        descriptionTxtField.setColumns(20);
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
@@ -21,5 +35,14 @@ public class InfoWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void setItem(TopSecretPieceBE item) {
+        this.item = item;
+        if (this.item != null) {
+            usernameTxtField.setText(this.item.getUsername());
+            passwordTxtField.setText(this.item.getPasswd());
+            descriptionTxtField.setText(this.item.getInfo().orElse("no description specified"));
+        }
     }
 }
--- src/main/java/dha/lockrepo/view/swing/WindowManager.java
+++ src/main/java/dha/lockrepo/view/swing/WindowManager.java
@@ -1,36 +1,54 @@
 package dha.lockrepo.view.swing;
 
+import dha.lockrepo.business.TopSecretService;
+import dha.lockrepo.business.TopSecretServiceImpl;
+import dha.lockrepo.core.domains.TopSecretBE;
+import dha.lockrepo.core.domains.TopSecretPieceBE;
+
 import javax.swing.*;
 
 public class WindowManager {
 
+    private JFrame mainWindowFrame;
+    private JFrame infoWindowFrame;
+    private MainWindow mainWindow;
+    private InfoWindow infoWindow;
+
+    private TopSecretService topSecretService;
     public enum WindowType {
         MAIN,
         INFO
     }
 
     public WindowManager() {
-        createJFrame("LockRepo", WindowType.MAIN);
+        topSecretService = new TopSecretServiceImpl();
+        mainWindowFrame = createJFrame("LockRepo", WindowType.MAIN);
+        mainWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindowFrame.setVisible(true);
     }
 
     void openInfoWindow(String selectedItem) {
-        createJFrame(selectedItem, WindowType.INFO);
+        infoWindowFrame = createJFrame(selectedItem, WindowType.INFO);
+        TopSecretPieceBE item = topSecretService.findPieceById(90L);
+        infoWindow.setItem(item);
+        infoWindowFrame.setVisible(true);
     }
 
-    private void createJFrame(String title, WindowType window) {
+    private JFrame createJFrame(String title, WindowType window) {
         JFrame frame = new JFrame(title);
         JPanel contentPane = null;
         switch (window) {
         case MAIN:
-            contentPane = new MainWindow(this).getMainPanel();
+            mainWindow = new MainWindow(this);
+            contentPane = mainWindow.getMainPanel();
             break;
         case INFO:
-            contentPane = new InfoWindow().getMainPanel();
+            infoWindow = new InfoWindow();
+            contentPane = infoWindow.getMainPanel();
             break;
         }
         frame.setContentPane(contentPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
+        return frame;
     }
 }
